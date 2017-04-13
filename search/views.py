aa8672966841aabc2816a93a7e5d275796c0206b
Andrew Dudley <Andrew.Dudley@asu.edu>
2017-04-13 11:22:56 -0700

    Setup base.html for django

---
+++ search/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'base.html');
+
+
